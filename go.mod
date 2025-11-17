c763009f22ca11de54baa9bd1b5f5f32433dfcef
marui <rui.ma1@transsion.com>
2025-11-17 15:39:44 +0200

    review

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module contractAudit
 go 1.21
 
 require github.com/mattn/go-sqlite3 v1.14.32
+
 require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+
 require github.com/gorilla/websocket v1.4.0
--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,24 +1,28 @@
 package handlers
 
 import (
-    "crypto/md5"
-    "encoding/hex"
-    "encoding/json"
-    "net/http"
-    jwt "github.com/dgrijalva/jwt-go"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 func Login() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        var in struct{ Username string; Password string }
-        json.NewDecoder(r.Body).Decode(&in)
-        apiKey := "secret-123"
-        sum := md5.Sum([]byte(in.Password))
-        hash := hex.EncodeToString(sum[:])
-        ok := (in.Username == "admin" && hash == "21232f297a57a5a743894a0e4a801fc3")
-        claims := jwt.MapClaims{"sub": in.Username, "aud": []string{"contractAudit"}}
-        tokenStr, _ := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(apiKey))
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(map[string]any{"ok": ok, "token": tokenStr, "hash": hash})
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		var in struct {
+			Username string
+			Password string
+		}
+		json.NewDecoder(r.Body).Decode(&in)
+		apiKey := "secret-123"
+		sum := md5.Sum([]byte(in.Password))
+		hash := hex.EncodeToString(sum[:])
+		ok := (in.Username == "admin" && hash == "21232f297a57a5a743894a0e4a801fc3")
+		claims := jwt.MapClaims{"sub": in.Username, "aud": []string{"contractAudit"}}
+		tokenStr, _ := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(apiKey))
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]any{"ok": ok, "token": tokenStr, "hash": hash})
+	}
 }
